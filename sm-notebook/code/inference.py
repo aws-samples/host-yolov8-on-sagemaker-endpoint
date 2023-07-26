a467b1a87e740cb5a81914b9da4e4503aa31c772
Michael Imstepf <michael.imstepf@gmail.com>
2023-07-26 13:19:44 +1000

    Predict with image URLs and image files

--- sm-notebook/code/inference.py
+++ sm-notebook/code/inference.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch, os, json, io, cv2, time
 from ultralytics import YOLO
+from PIL import Image
+import requests
 
 def model_fn(model_dir):
     print("Executing model_fn from inference.py ...")
@@ -10,32 +12,61 @@ def model_fn(model_dir):
 
 def input_fn(request_body, request_content_type):
     print("Executing input_fn from inference.py ...")
-    if request_content_type:
-        jpg_original = np.load(io.BytesIO(request_body), allow_pickle=True)
-        jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-        img = cv2.imdecode(jpg_as_np, flags=-1)
+    if request_content_type.lower() == "image/png" or request_content_type.lower() == "image/jpg" or request_content_type.lower() == "image/jpeg":
+        # jpg_original = np.load(io.BytesIO(request_body), allow_pickle=True)
+        # jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+        # img = cv2.imdecode(jpg_as_np, flags=-1)
+        image = Image.open(io.BytesIO(request_body))
+        image = np.asarray(image)
+        return image        
+    elif request_content_type.lower() == "application/jsonlines" or request_content_type.lower == "application/jsonlines":
+        # request_body = b'{"image_url":"https://www.addictivetips.com/app/uploads/2017/10/ios-privacy-settings.jpg"}'    
+        print("request_body:")
+        print(request_body)
+        request_body_fix_single_quotes = request_body.replace(b"'", b'"')
+        print("request_body_fix_single_quotes:")
+        print(request_body_fix_single_quotes)
+        request_body_json = json.load(io.BytesIO(request_body_fix_single_quotes))
+        print("request_body_json:")
+        print(request_body_json)
+        image_url = request_body_json['image_url']    
+        print("image_url:")
+        print(image_url)
+        response = requests.get(image_url)
+        image = Image.open(io.BytesIO(response.content))
+        image = np.asarray(image)
+        return image        
     else:
         raise Exception("Unsupported content type: " + request_content_type)
-    return img
     
 def predict_fn(input_data, model):
     print("Executing predict_fn from inference.py ...")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
     with torch.no_grad():
-        result = model(input_data)
+        result = model(input_data, imgsz=1760)
     return result
         
 def output_fn(prediction_output, content_type):
     print("Executing output_fn from inference.py ...")
-    infer = {}
-    for result in prediction_output:
-        if 'boxes' in result.keys:
-            infer['boxes'] = result.boxes.numpy().data.tolist()
-        if 'masks' in result.keys:
-            infer['masks'] = result.masks.numpy().data.tolist()
-        if 'keypoints' in result.keys:
-            infer['keypoints'] = result.keypoints.numpy().data.tolist()
-        if 'probs' in result.keys:
-            infer['probs'] = result.probs.numpy().data.tolist()
-    return json.dumps(infer)
+    # YOLO can handle multiple images, get predictions for first image
+    results_for_first_image = prediction_output[0]
+
+    # Create empty NumPy array
+    rows, cols = 0, 5
+    predictions = np.empty((rows, cols))
+
+    # Iterate through predictions
+    for result in results_for_first_image:
+        # Get box from object and convert from TensorFlow to NumPy array
+        box = result.boxes[0].numpy()
+        # Extract coordinates (first row)
+        coordinates = box.xywhn[0, :]
+        # Get label class from object
+        cls = int(box.cls)
+        # Stitch coordinates and label calls together in one array
+        prediction = np.append(coordinates, cls)
+        # Append prediction to multi row array
+        print(prediction)
+        predictions = np.vstack((predictions, prediction))    
+    return json.dumps(predictions.tolist())
